c9449bd808814870a10855d6688a584724667481
EssaadaniYounes <younesessaadani2003@gmail.com>
2022-05-31 17:31:30 +0200

    after remove ssr

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,43 +4,53 @@ import { useUserContext } from '../../context/user'
 import { Statistics } from '../../components/parts';
 import fetch from '../../helpers/fetch-data';
 
-function Dashboard({ dataUser, sales, clients, apartments, payments }) {
+function Dashboard() {
 
-  const { setUser, setSales, setClients, setApartments, setPayments } = useUserContext();
+  const { setUser, user, setSales, setClients, setApartments, setPayments } = useUserContext();
 
   useEffect(() => {
-    if (dataUser) {
-      setUser(dataUser);
-      setSales(sales);
-      setClients(clients);
-      setApartments(apartments);
-      setPayments(payments);
+    async function getServerSideProps() {
+      const token = localStorage.getItem('token');
+      const response = await fetch('sales', 'get', {}, token);
+      const apartments = await fetch('properties', 'get', {}, token);
+      const payments = await fetch('payments', 'get', {}, token);
+      const clients = await fetch('clients', 'get', {}, token);
+      setUser({
+        email: 'younes@gmail.com',
+        password: '1234',
+        name: 'Younes',
+      });
+      setSales(response.data);
+      setClients(clients.data);
+      setApartments(apartments.data);
+      setPayments(payments.data);
     }
+    getServerSideProps();
   }, []);
 
   return (
     <div>
-      <Statistics />
+      {user && <Statistics />}
     </div>
   )
 }
 
-export async function getServerSideProps(ctx) {
-  const token = ctx.req.cookies != undefined ? ctx.req.cookies.token : '';
-  const response = await fetch('sales', 'get', {}, token);
-  const apartments = await fetch('properties', 'get', {}, token);
-  const payments = await fetch('payments', 'get', {}, token);
-  const clients = await fetch('clients', 'get', {}, token);
-  const data = await AutoLogin(ctx);
-  return {
-    props: {
-      dataUser: data.dataUser,
-      sales: response.data,
-      clients: clients.data,
-      apartments: apartments.data,
-      payments: payments.data
-    }
-  }
-}
+// export async function getServerSideProps(ctx) {
+//   const token = ctx.req.cookies != undefined ? ctx.req.cookies.token : '';
+//   const response = await fetch('sales', 'get', {}, token);
+//   const apartments = await fetch('properties', 'get', {}, token);
+//   const payments = await fetch('payments', 'get', {}, token);
+//   const clients = await fetch('clients', 'get', {}, token);
+//   const data = await AutoLogin(ctx);
+//   return {
+//     props: {
+//       dataUser: data.dataUser,
+//       sales: response.data,
+//       clients: clients.data,
+//       apartments: apartments.data,
+//       payments: payments.data
+//     }
+//   }
+// }
 
 export default Dashboard
