1170c1ba42652c7edb79e33e31aff8b43c1a9272
EssaadaniYounes <younesessaadani2003@gmail.com>
2022-06-19 20:17:16 +0200

    add redirecting

--- pages/admin/lodgings/plan/[id].js
+++ pages/admin/lodgings/plan/[id].js
@@ -36,8 +36,8 @@ function plan({ lodging, dataUser }) {
                 </Link>
             </div>
 
-            <object ref={object} width="600" height="500">
-                <embed ref={embed} width="600px" height="500px" />
+            <object ref={object} className='w-full '>
+                <embed ref={embed} className='w-full' />
                 <p>This browser does not support PDFs. Please download the PDF to view it: <a href={lodging.plan}>View the PDF</a>.</p>
             </object>
 
